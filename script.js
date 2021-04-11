7b2af69ab00be29c98fe3d52ef54f6a13db39dfe
Tyler Hance <tyler.hance@gmail.com>
2021-04-11 16:36:14 -0700

    Building out array for numbers/special char and lowercase to uppercase.

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ var generateBtn = docment.querySelector("#generate");
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-
     passwordText.value = password;
-
 }
 
 // Add Event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Array
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "="];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Array for lower case to upper case
+var upperCase = lowerCase.map(lowerCase => lowerCase.toUpperCase());
