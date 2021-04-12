11349f7d3a872f51cd97cbe89e56137e26e27e03
Tyler Hance <tyler.hance@gmail.com>
2021-04-11 18:13:19 -0700

    Writing password generator alert and prompts.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = docment.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 //Write password to the #password input
 function writePassword() {
@@ -12,9 +12,41 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Array
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "="];
+/*var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "="];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+*/
+
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericChar = "0123456789";
+var specialChar = "!@#$%^&*()_+-=<>?,./:;'{}[]\|";
 
 // Array for lower case to upper case
-var upperCase = lowerCase.map(lowerCase => lowerCase.toUpperCase());
+//var upperCase = lowerCase.map(lowerCase => lowerCase.toUpperCase());
+
+// Main function to generate password
+function generatePassword(){
+    var password = "";
+    var passwordChar = "";
+}
+
+//Prompt for user to select password length
+var passwordLength = prompt("How long would you like your password to be? Must be between 8-128 characters.");
+passwordLength = parseInt(passwordLength);
+
+// Validate the user input
+if(passwordLength < 8) {
+    alert("Invalid input! Password must be at least 8 characters!");
+}
+
+if(passwordLength > 128) {
+    alert("Invalid input! Password can't exceed 128 characters!");
+}
+
+// Confirms lowercase true/false
+var lowercase = confirm("Would you like lowercase characters?");
+
+if(lowercase) {
+    passwordChar += lowercaseChar;
+}
