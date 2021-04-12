e6f863ffbc460251b2e317a7b25789263c645817
Tyler Hance <tyler.hance@gmail.com>
2021-04-11 20:42:20 -0700

    Fixing math.random logic for password generator.

--- script.js
+++ script.js
@@ -16,9 +16,6 @@ var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-
-
-
 // Array for lower case to upper case
 var upperCase = lowerCase.map(lowerCase => lowerCase.toUpperCase());
 
@@ -26,3 +23,16 @@ var upperCase = lowerCase.map(lowerCase => lowerCase.toUpperCase());
 function generatePassword(){
     var password = "";
 }
+var passLength = Math.floor(Math.random() * password.length);
+//Prompting user input for password
+var userInput = window.prompt("How many characters will your password be? Must be 8-128 characters!");
+
+if (passLength < 8) {
+    window.alert("Invalid input! Password must be AT LEAST 8 characters!");
+};
+
+if (passLength > 128) {
+    window.alert("Invalid input! Password can't exceed 128 characters!");
+};
+
+
