ac647e41c932afbfbd477f464cc23d76084e26c7
Tyler Hance <tyler.hance@gmail.com>
2021-04-13 22:39:11 -0700

    Addin logic for uppercase/lowercase.

--- script.js
+++ script.js
@@ -5,16 +5,12 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
+  
     passwordText.value = password;
-}
+  }
 
 // Add Event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// Array
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "="];
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Array for lower case to upper case
 //var upperCase = lowerCase.map(lowerCase => lowerCase.toUpperCase());
@@ -24,6 +20,7 @@ var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numberChar = "0123456789";
 var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
+var specialCheck = "";
 var passwordLength = "";
 var uppercaseCheck = "";
 var numberCheck = "";
@@ -36,11 +33,30 @@ function chooseLength() {
     );
 // Verify the user input is valid
 if (passwordLength < 8) {
-    alert("Password length must be a number between 8-128 characters");
-    determineLength();
-}
-if (passwordLength > 128) {
-    alert("Invalid input! Password must be no more than 128 characters!");
+    alert("Invalid Input! Password length must be a number between 8-128 characters!");
     chooseLength();
 }
+if (passwordLength > 128) {
+    alert("Invalid input! Password length must be a number between 8-128 characters");
+    chooseLength();
+}
+return passwordLength;
+}
+
+// Function asking user if they want uppercase characters
+function chooseUpper() {
+    uppercaseCheck = prompt("Do you want uppercase letters in your password? \n(Yes or No)");
+
+    uppercaseCheck = uppercaseCheck.toLowerCase();
+    
+    if (uppercaseCheck === "yes" || uppercaseCheck === "y") {
+        uppercaseCheck = true;
+        return uppercaseCheck;
+    }
+    if (uppercaseCheck === "no" || uppercaseCheck === "n") {
+        uppercaseCheck = false;
+        return uppercaseCheck;
+        chooseUpper();
+    }
+    return uppercaseCheck;
 }
